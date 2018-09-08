569b7ecba1cacd6f52c1a70935ca6f81a4544449
groszek20 <pea@op.pl>
2018-09-08 22:37:24 +0200

    edip post

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -1,46 +1,44 @@
 <?php
-if (isset($_POST['create_post'])) {
-    $post_title = $_POST['title'];
-    $post_author = $_POST['author'];
-    $post_category_id = $_POST['post_category_id'];
-    $post_content = $_POST['post_content'];
-    $post_date = date('d-m-y');
-    $post_comment_count = 4;
-    $post_image = $_FILES['image']['name'];
-    $post_image_temp = $_FILES['image']['tmp_name'];
-    $post_status = $_POST['post_status'];
-    $post_tags = $_POST['post_tags'];
+if (isset($_GET['p_id'])) {
+    $the_post_id = $_GET['p_id'];
+}
 
-    move_uploaded_file($post_image_temp, "../images/$post_image");
+$query = "SELECT * FROM `posts` ";
+$select_posts_by_id = mysqli_query($connection, $query);
 
-    $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_comment_count, post_status) "
-    ."VALUE({$post_category_id}, '{$post_title}', '{$post_author}', now(), '{$post_image}', '{$post_content}', '{$post_tags}', '{$post_comment_count}', '{$post_status}')";
-
-    $create_post_query = mysqli_query($connection, $query);
-
-    confirm($create_post_query);
+while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
+    $post_id = $row['post_id'];
+    $post_author = $row['post_author'];
+    $post_title = $row['post_title'];
+    $post_date = $row['post_date'];
+    $post_image = $row['post_image'];
+    $post_tags = $row['post_tags'];
+    $post_status = $row['post_status'];
+    $post_content = $row['post_content'];
+    $post_category_id = $row['post_category_id'];
+    $post_comment_count = $row['post_comment_count'];
 }
 ?>
 
 <form action="" method="post" enctype="multipart/form-data">
     <div class="form-group">
         <label for="title">Post Title</label>
-        <input type="text" class="form-control" name="title">
+        <input value="<?php echo $post_title;?>" type="text" class="form-control" name="title">
     </div>
 
     <div class="form-group">
         <label for="post_category">Category Id</label>
-        <input type="text" class="form-control" name="post_category_id">
+        <input value="<?php echo $post_id;?>" type="text" class="form-control" name="post_category_id">
     </div>
 
     <div class="form-group">
         <label for="post_author">Post Author</label>
-        <input type="text" class="form-control" name="author">
+        <input value="<?php echo $post_author;?>" type="text" class="form-control" name="author">
     </div>
 
     <div class="form-group">
         <label for="post_status">Post Status</label>
-        <input type="text" class="form-control" name="post_status">
+        <input value="<?php echo $post_status;?>" type="text" class="form-control" name="post_status">
     </div>
 
     <div class="form-group">
@@ -50,12 +48,13 @@ if (isset($_POST['create_post'])) {
 
     <div class="form-group">
         <label for="post_tags">Post Tags</label>
-        <input type="text" class="form-control" name="post_tags">
+        <input value="<?php echo $post_tags;?>" type="text" class="form-control" name="post_tags">
     </div>
 
     <div class="form-group">
         <label for="post_content">Post Content</label>
         <textarea class="form-control" name="post_content" id="" cols="30" rows="10">
+            <?php echo $post_content;?>"
         </textarea>
     </div>
 
--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -40,6 +40,7 @@
                 echo "<td>$post_comment_count</td>";
                 echo "<td>$post_date</td>";
                 echo "<td><a href='posts.php?delete={$post_id}'>Delete</a></td>";
+                echo "<td><a href='posts.php?source=edit_post&p_ip={$post_id}'>Edit</a></td>";
                 echo "</tr>";
             }
             ?>
