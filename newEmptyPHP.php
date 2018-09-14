476e6bea38426921877763876974762338ddd7b4
groszek20 <pea@op.pl>
2018-09-14 20:57:03 +0200

    post

--- newEmptyPHP.php
+++ newEmptyPHP.php
@@ -1,56 +1,3 @@
-<?php include 'includes/db.php';?>
-<?php include "includes/header.php";?>   
-<!-- Navigation -->
-
-<?php include "includes/navigation.php";?>   
-
-<!-- Page Content -->
-<div class="container">
-
-    <div class="row">
-
-        <!-- Blog Entries Column -->
-        <div class="col-md-8">
-
-            <h1 class="page-header">
-                Page Heading
-                <small>Secondary Text</small>
-            </h1>
-
-            <?php
-            if(isset($_GET['p_id'])){
-                $the_post_id = $_GET['p_id'];
-            }
-            $query = "select * from posts WHERE post_id = ".$the_post_id;
-            $result = mysqli_query($connection, $query);
-
-            while ($row = mysqli_fetch_assoc($result)) {
-                $post_id = $row['post_id'];
-                $post_title = $row['post_title'];
-                $post_author = $row['post_author'];
-                $post_date = $row['post_date'];
-                $post_content = $row['post_content'];
-                $post_image = $row['post_image'];
-                $post_tags = $row['post_tags'];
-                ?>
-                <!-- First Blog Post -->
-                <h2>
-                    <a href="post.php?p_id=<?php echo $post_id;?>"><?php echo $post_title;?></a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php"><?php echo $post_author;?></a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date;?></p>
-                <hr>
-                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
-                <hr>
-                <p><?php echo $post_content;?></p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-                <hr>
-                <?php
-            }
-            ?>
  <!-- Blog Comments -->
 
                 <!-- Comments Form -->
@@ -106,15 +53,3 @@
                         <!-- End Nested Comment -->
                     </div>
                 </div>
-
-        </div>
-
-        <!-- Blog Sidebar Widgets Column -->
-        <?php include "includes/sidebar.php";?>
-
-    </div>
-    <!-- /.row -->
-
-    <hr>
-    <!-- Footer -->
-    <?php include "includes/footer.php";?>
