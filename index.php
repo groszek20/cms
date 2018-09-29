7461e82e234f7ba6294883e3de8c707380f5731b
groszek20 <pea@op.pl>
2018-09-29 22:24:12 +0200

    status update

--- index.php
+++ index.php
@@ -26,25 +26,32 @@
                 $post_title = $row['post_title'];
                 $post_author = $row['post_author'];
                 $post_date = $row['post_date'];
-                $post_content = substr($row['post_content'], 0,100);
+                $post_content = substr($row['post_content'], 0, 100);
                 $post_image = $row['post_image'];
-                $post_tags = $row['post_tags'];
-                ?>
-                <!-- First Blog Post -->
-                <h2>
-                    <a href="post.php?p_id=<?php echo $post_id;?>"><?php echo $post_title;?></a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php"><?php echo $post_author;?></a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date;?></p>
-                <hr>
-                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
-                <hr>
-                <p><?php echo $post_content;?></p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                $post_status = $row['post_status'];
+                if ($post_status !== 'published') {
+                    echo "<h1 class='text-center'>NO POSTS, SORRY</h1>";
+                } else {
+                    ?>
+                    <!-- First Blog Post -->
+                    <h2>
+                        <a href="post.php?p_id=<?php echo $post_id;?>"><?php echo $post_title;?></a>
+                    </h2>
+                    <p class="lead">
+                        by <a href="index.php"><?php echo $post_author;?></a>
+                    </p>
+                    <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date;?></p>
+                    <hr>
+                    <img class="img-responsive" src="images/<?php echo $post_image;?>" alt="">
+                    <hr>
+                    <p><?php echo $post_content;?></p>
+                    <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
-                <hr>
+                    <hr>
+                    <?php
+                }
+                ?>
+
                 <?php
             }
             ?>
