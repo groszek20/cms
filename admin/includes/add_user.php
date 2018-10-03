d19fe5a853b091e85133b2b31f83a72f8a7d8285
groszek20 <pea@op.pl>
2018-10-03 22:25:26 +0200

    add users

--- admin/includes/add_user.php
+++ admin/includes/add_user.php
@@ -1,73 +1,64 @@
 <?php
 include '../admin/functions.php';
 
-if (isset($_POST['create_post'])) {
-    $post_title = $_POST['title'];
-    $post_author = $_POST['author'];
-    $post_category_id = $_POST['post_category_id'];
-    $post_content = $_POST['post_content'];
-    $post_date = date('d-m-y');
-    $post_image = $_FILES['image']['name'];
-    $post_image_temp = $_FILES['image']['tmp_name'];
-    $post_status = $_POST['post_status'];
-    $post_tags = $_POST['post_tags'];
+if (isset($_POST['create_user'])) {
+//    $user_id = $_POST['user_id'];
+    $user_firstname = $_POST['user_firstname'];
+    $user_lastname = $_POST['user_lastname'];
+    $user_password = $_POST['user_password'];
+    $username = $_POST['username'];
+    $user_email = $_POST['user_email'];
+    $user_role = $_POST['user_role'];
+   
 
-    move_uploaded_file($post_image_temp, "../images/$post_image");
+//    move_uploaded_file($post_image_temp, "../images/$post_image");
 
-    $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_status) "
-    ."VALUE({$post_category_id}, '{$post_title}', '{$post_author}', now(), '{$post_image}', '{$post_content}', '{$post_tags}','{$post_status}')";
+    $query = "INSERT INTO users (username, user_password, user_firstname, user_lastname, user_email, user_role) "
+    ."VALUE('{$username}', '{$user_password}', '{$user_firstname}', '{$user_lastname}', '{$user_email}', '{$user_role}')";
 
-    $create_post_query = mysqli_query($connection, $query);
-    confirmQuery($create_post_query);
+    echo $query;
+    $create_user_query = mysqli_query($connection, $query);
+    confirmQuery($create_user_query);
 }
 ?>
 
 <form action="" method="post" enctype="multipart/form-data">
     <div class="form-group">
-        <label for="title">Firstname</label>
+        <label for="user_firstname">Firstname</label>
         <input type="text" class="form-control" name="user_firstname">
     </div>
 
     <div class="form-group">
-        <label for="post_author">Lastname</label>
+        <label for="user_lastname">Lastname</label>
         <input type="text" class="form-control" name="user_lastname">
     </div>
 
     <div class="form-group">
-        <select name="post_category_id" id="post_category">
-
-            <?php
-            $query = "SELECT * FROM `users`";
-            $select_users = mysqli_query($connection, $query);
-            confirmQuery($select_users);
-            while ($row = mysqli_fetch_assoc($select_users)) {
-                $user_id = $row['user_id'];
-                $user_role = $row['user_role'];
-                echo "<option value='{$user_id}'>{$user_role}</option>";
-            }
-            ?>     
+        <select name="user_role" id="">
+            <option value="admin">Admin</option>
+            <option value="subscriber">Subscriber</option>
         </select>
     </div>
 
     <div class="form-group">
-        <label for="post_status">Password</label>
+        <label for="user_password">Password</label>
         <input type="password" class="form-control" name="user_password">
     </div>
 
     <div class="form-group">
-        <label for="post_tags">Username</label>
+        <label for="username">Username</label>
         <input type="text" class="form-control" name="username">
     </div>
 
     <div class="form-group">
-        <label for="post_tags">E-Mail</label>
+        <label for="user_email">E-Mail</label>
         <input type="email" class="form-control" name="user_email">
     </div>
 
-    <div class="form-group">
-        <label for="post_image">User Image</label>
+<!--    <div class="form-group">
+        <label for="user_image">User Image</label>
         <input type="file" name="user_image">
-    </div>
+    </div>-->
 
     <div class="form-group">
         <input class="btn btn-primary" type="submit" name="create_user" value="Add User">
