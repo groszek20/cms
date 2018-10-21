6a380a16a46761757604d378731a234267295940
groszek20 <pea@op.pl>
2018-10-21 13:30:40 +0200

    bug fix

--- index.php
+++ index.php
@@ -29,9 +29,7 @@
                 $post_content = substr($row['post_content'], 0, 100);
                 $post_image = $row['post_image'];
                 $post_status = $row['post_status'];
-                if ($post_status !== 'published') {
-                    echo "<h1 class='text-center'>NO POSTS, SORRY</h1>";
-                } else {
+                if ($post_status == 'published') {
                     ?>
                     <!-- First Blog Post -->
                     <h2>
