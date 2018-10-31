0eac72f13bfff9f892e88c67494ab267a5a8c08f
groszek20 <pea@op.pl>
2018-10-31 23:17:30 +0100

    update

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -1,76 +1,80 @@
 <div class="col-lg-12">
-    
+
     <form action="" method='post'>
-    <table class="table table-bordered table-hover">
-        
-        <div id="bulkOptionsContainer" class="col-xs-4">
-            
-            <select class="form-control" name="" id="">
-                <option value="">Publish</option>
-                <option value="">Draft</option>
-                <option value="">Delete</option>
-            </select>
-        </div>
-        <div class="col-xs-4">
-            <input type="submit" name="submit" class="btn btn-success" value="Apply">
-            <a class="btn btn-primary" a href="add_post.php">Add New</a>
-        </div>
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Author</th>
-                <th>Title</th>
-                <th>Category</th>
-                <th>Status</th>
-                <th>Image</th>
-                <th>Tags</th>
-                <th>Comments</th>
-                <th>Date</th>
-                <th>Delete</th>
-                <th>Edit</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            global $connection;
-            $query = "SELECT * FROM `posts` ";
-            $select_posts = mysqli_query($connection, $query);
-            while ($row = mysqli_fetch_assoc($select_posts)) {
-                $post_id = $row['post_id'];
-                $post_author = $row['post_author'];
-                $post_title = $row['post_title'];
-                $post_date = $row['post_date'];
-                $post_image = $row['post_image'];
-                $post_tags = $row['post_tags'];
-                $post_status = $row['post_status'];
-                $post_content = $row['post_content'];
-                $post_category_id = $row['post_category_id'];
-                $post_comment_count = $row['post_comment_count'];
-                echo "<tr>";
-                echo "<td>$post_id</td>";
-                echo "<td>$post_author</td>";
-                echo "<td>$post_title</td>";
+        <table class="table table-bordered table-hover">
 
-                $query_title = "SELECT * FROM `category` WHERE `cat_id` = $post_category_id  ";
-                $select_categories = mysqli_query($connection, $query_title);
-                while ($row = mysqli_fetch_assoc($select_categories)) {
-                    $cat_title = $row['cat_title'];
-                    echo "<td>$cat_title</td>";
+            <div id="bulkOptionsContainer" class="col-xs-4">
+
+                <select class="form-control" name="" id="">
+                    <option value="">Publish</option>
+                    <option value="">Draft</option>
+                    <option value="">Delete</option>
+                </select>
+            </div>
+            <div class="col-xs-4">
+                <input type="submit" name="submit" class="btn btn-success" value="Apply">
+                <a class="btn btn-primary" a href="add_post.php">Add New</a>
+            </div>
+            <thead>
+                <tr>
+                    <th><input id="selectAllBoxes" type="checkbox" value='<?php echo $post_id; ?>'></th>
+                    <th>Id</th>
+                    <th>Author</th>
+                    <th>Title</th>
+                    <th>Category</th>
+                    <th>Status</th>
+                    <th>Image</th>
+                    <th>Tags</th>
+                    <th>Comments</th>
+                    <th>Date</th>
+                    <th>Delete</th>
+                    <th>Edit</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                global $connection;
+                $query = "SELECT * FROM `posts` ";
+                $select_posts = mysqli_query($connection, $query);
+                while ($row = mysqli_fetch_assoc($select_posts)) {
+                    $post_id = $row['post_id'];
+                    $post_author = $row['post_author'];
+                    $post_title = $row['post_title'];
+                    $post_date = $row['post_date'];
+                    $post_image = $row['post_image'];
+                    $post_tags = $row['post_tags'];
+                    $post_status = $row['post_status'];
+                    $post_content = $row['post_content'];
+                    $post_category_id = $row['post_category_id'];
+                    $post_comment_count = $row['post_comment_count'];
+                    echo "<tr>";
+                    ?>
+                <td><input class='checkBoxes' type='checkbox' name='checkBoxArray[]'></td>
+                    <?php
+                    echo "<td>$post_id</td>";
+                    echo "<td>$post_author</td>";
+                    echo "<td>$post_title</td>";
+
+                    $query_title = "SELECT * FROM `category` WHERE `cat_id` = $post_category_id  ";
+                    $select_categories = mysqli_query($connection, $query_title);
+                    while ($row = mysqli_fetch_assoc($select_categories)) {
+                        $cat_title = $row['cat_title'];
+                        echo "<td>$cat_title</td>";
+                    }
+
+                    echo "<td>$post_status</td>";
+                    echo "<td><img width='100' src='../images/$post_image' alt='image'></td>";
+                    echo "<td>$post_tags</td>";
+                    echo "<td>$post_comment_count</td>";
+                    echo "<td>$post_date</td>";
+                    echo "<td><a href='posts.php?delete={$post_id}'>Delete</a></td>";
+                    echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
+                    echo "</tr>";
                 }
-
-                echo "<td>$post_status</td>";
-                echo "<td><img width='100' src='../images/$post_image' alt='image'></td>";
-                echo "<td>$post_tags</td>";
-                echo "<td>$post_comment_count</td>";
-                echo "<td>$post_date</td>";
-                echo "<td><a href='posts.php?delete={$post_id}'>Delete</a></td>";
-                echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
-                echo "</tr>";
-            }
-            ?>
-        </tbody>
-    </table>
-        </form>
+                ?>
+            </tbody>
+        </table>
+    </form>
 </div>
 
 <?php
