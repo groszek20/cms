7bf59d62149e12b1502e04f4aa9e7c78902c41f9
groszek20 <pea@op.pl>
2018-11-01 22:52:45 +0100

    bulk options

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -1,14 +1,27 @@
 <div class="col-lg-12">
-
+<?php 
+include 'functions.php';
+if(isset($_POST['checkBoxArray']))
+    foreach ($_POST['checkBoxArray'] as $post_id ){
+        $bulk_options = $_POST['bulk_options'];
+        switch ($bulk_options){
+            case 'published';
+                $query = "UPDATE posts SET post_status = '{$bulk_options}' WHERE post_id= {$post_id}";
+                $update_to_published_status = mysqli_query($connection, $query);
+                confirmQuery($update_to_published_status);
+                break;
+        }
+    }
+?>
     <form action="" method='post'>
         <table class="table table-bordered table-hover">
 
             <div id="bulkOptionsContainer" class="col-xs-4">
 
-                <select class="form-control" name="" id="">
-                    <option value="">Publish</option>
-                    <option value="">Draft</option>
-                    <option value="">Delete</option>
+                <select class="form-control" name="bulk_options" id="">
+                    <option value="published">Publish</option>
+                    <option value="draft">Draft</option>
+                    <option value="delete">Delete</option>
                 </select>
             </div>
             <div class="col-xs-4">
@@ -49,7 +62,7 @@
                     $post_comment_count = $row['post_comment_count'];
                     echo "<tr>";
                     ?>
-                <td><input class='checkBoxes' type='checkbox' name='checkBoxArray[]'></td>
+                <td><input class='checkBoxes' type='checkbox' name='checkBoxArray[]' value='<?php echo $post_id; ?>'></td>
                     <?php
                     echo "<td>$post_id</td>";
                     echo "<td>$post_author</td>";
